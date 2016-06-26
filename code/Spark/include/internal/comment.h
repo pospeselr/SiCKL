3c16003f166f7ff73890a93823b777078b0d5980
Richard Pospesel <pospeselr@gmail.com>
2016-06-26 14:07:30 -0700

    adding the new comment.h header

---
+++ code/Spark/include/internal/comment.h
@@ -0,0 +1,12 @@
+#pragma once
+namespace Spark
+{
+	void Comment(const char* comment)
+	{
+		// create comment node
+		Node* commentNode = spark_create_comment_node(comment);
+		Node* currentScope = spark_peek_scope_node();
+		spark_add_child_node(currentScope, commentNode);
+
+	}
+}
