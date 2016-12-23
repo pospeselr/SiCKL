4fd5d8bf991cf9b915d0361b72d680ad6ee6f95f
Richard Pospesel <pospeselr@gmail.com>
2016-12-23 14:29:43 -0800

    begin better text generation, created doSnprintf overload which explicitly allows solely string literals (which would error out when passed directly into snprintf)

--- code/Spark/source/text_utilities.hpp
+++ code/Spark/source/text_utilities.hpp
@@ -4,8 +4,17 @@ namespace Spark
 {
     namespace Internal
     {
-        template<typename... Args>
-        int32_t doSnprintf(char* buffer, int32_t buffer_size, int32_t written, const Args&... args)
+        template<typename T>
+        struct codegen_context
+        {
+            char* buffer = nullptr;
+            int32_t capacity = 0;
+            int32_t written = 0;
+            T context;
+        };
+
+        template<int32_t N, typename... Args>
+        int32_t doSnprintf(char* buffer, int32_t buffer_size, int32_t written, const char (&format)[N], const Args... args)
         {
             auto nullOrOffset = [](char* buffer, uint32_t offset) -> char*
             {
@@ -13,12 +22,40 @@ namespace Spark
             };
 
             SPARK_ASSERT(written >= 0);
-            auto len = snprintf(nullOrOffset(buffer, written),  buffer_size == 0 ? 0 : buffer_size - written, args...);
+            auto len = snprintf(nullOrOffset(buffer, written),  buffer_size == 0 ? 0 : buffer_size - written, format, args...);
 
             SPARK_ASSERT(len >= 0);
             SPARK_ASSERT(buffer_size == 0 || (written + len < buffer_size));
 
             return (written + len);
+        }
+
+        template<int32_t N>
+        int32_t doSnprintf(char* buffer, int32_t buffer_size, int32_t written, const char (&literal)[N])
+        {
+            // N includes terminating null, which we don't count here
+            const auto len = N - 1;
+            if(buffer == nullptr)
+            {
+                SPARK_ASSERT(buffer_size == 0);
+            }
+            else
+            {
+                SPARK_ASSERT(written + len < buffer_size);
+                memcpy(buffer + written, (void*)literal, (N));
+            }
+            return (written + len);
+        }
+
+        template<typename Context, int32_t N, typename... Args>
+        void doSnprintf(codegen_context<Context>& ctx, const char (&format)[N], const Args... args)
+        {
+            ctx.written = doSnprintf(
+                ctx.buffer,
+                ctx.capacity,
+                ctx.written,
+                format,
+                args...);
         }
     }
 }
