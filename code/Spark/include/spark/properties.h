c9709d5e2f96907a3f63b0c26777025157d01f43
Richard Pospesel <pospeselr@gmail.com>
2017-02-05 13:39:51 -0800

    moved properties into Internal namespace

--- code/Spark/include/spark/properties.h
+++ code/Spark/include/spark/properties.h
@@ -21,71 +21,69 @@ namespace Spark
             auto currentScope = spark_peek_scope_node(Spark::Internal::ThrowOnError());
             spark_add_child_node(currentScope, assignmentNode, Spark::Internal::ThrowOnError());
         }
+
+        template<typename TYPE, Spark::Internal::Property ID>
+        struct property_r
+        {
+            __attribute__((always_inline))
+            operator const rvalue<TYPE>() const
+            {
+                const auto prop = static_cast<spark_property_t>(ID);
+                auto propertyNode = spark_create_property_node(prop, Spark::Internal::ThrowOnError());
+
+                const auto dt = static_cast<spark_datatype_t>(TYPE::type);
+                const auto op = static_cast<spark_operator_t>(Spark::Internal::Operator::Property);
+
+                return rvalue<TYPE>(spark_create_operator2_node(dt, op, this->_node, propertyNode));
+            };
+
+            __attribute__((always_inline))
+            const rvalue<TYPE> operator()() const
+            {
+                const auto prop = static_cast<spark_property_t>(ID);
+                auto propertyNode = spark_create_property_node(prop, Spark::Internal::ThrowOnError());
+
+                const auto dt = static_cast<spark_datatype_t>(TYPE::type);
+                const auto op = static_cast<spark_operator_t>(Spark::Internal::Operator::Property);
+
+                return rvalue<TYPE>(spark_create_operator2_node(dt, op, this->_node, propertyNode));
+            }
+            spark_node_t* _node;
+        };
+
+        template<typename TYPE, Spark::Internal::Property ID>
+        struct property_rw : public property_r<TYPE, ID>
+        {
+            __attribute__((always_inline))
+            operator lvalue<TYPE>()
+            {
+                const auto prop = static_cast<spark_property_t>(ID);
+                auto propertyNode = spark_create_property_node(prop, Spark::Internal::ThrowOnError());
+
+                const auto dt = static_cast<spark_datatype_t>(TYPE::type);
+                const auto op = static_cast<spark_operator_t>(Spark::Internal::Operator::Property);
+
+                return lvalue<TYPE>(spark_create_operator2_node(dt, op, this->_node, propertyNode));
+            };
+
+            __attribute__((always_inline))
+            lvalue<TYPE> operator()()
+            {
+                const auto prop = static_cast<spark_property_t>(ID);
+                auto propertyNode = spark_create_property_node(prop, Spark::Internal::ThrowOnError());
+
+                const auto dt = static_cast<spark_datatype_t>(TYPE::type);
+                const auto op = static_cast<spark_operator_t>(Spark::Internal::Operator::Property);
+
+                return lvalue<TYPE>(spark_create_operator2_node(dt, op, this->_node, propertyNode));
+            }
+
+            __attribute__((always_inline))
+            property_rw& operator=(const rvalue<TYPE>& right)
+            {
+                Internal::property_assignment_operator(ID, TYPE::type, this->_node, right._node);
+                return *this;
+            }
+        };
     }
-
-    using namespace Internal;
-
-    template<typename TYPE, Spark::Internal::Property ID>
-    struct property_r
-    {
-        __attribute__((always_inline))
-        operator const rvalue<TYPE>() const
-        {
-            const auto prop = static_cast<spark_property_t>(ID);
-            auto propertyNode = spark_create_property_node(prop, Spark::Internal::ThrowOnError());
-
-            const auto dt = static_cast<spark_datatype_t>(TYPE::type);
-            const auto op = static_cast<spark_operator_t>(Spark::Internal::Operator::Property);
-
-            return rvalue<TYPE>(spark_create_operator2_node(dt, op, this->_node, propertyNode));
-        };
-
-        __attribute__((always_inline))
-        const rvalue<TYPE> operator()() const
-        {
-            const auto prop = static_cast<spark_property_t>(ID);
-            auto propertyNode = spark_create_property_node(prop, Spark::Internal::ThrowOnError());
-
-            const auto dt = static_cast<spark_datatype_t>(TYPE::type);
-            const auto op = static_cast<spark_operator_t>(Spark::Internal::Operator::Property);
-
-            return rvalue<TYPE>(spark_create_operator2_node(dt, op, this->_node, propertyNode));
-        }
-        spark_node_t* _node;
-    };
-
-    template<typename TYPE, Spark::Internal::Property ID>
-    struct property_rw : public property_r<TYPE, ID>
-    {
-        __attribute__((always_inline))
-        operator lvalue<TYPE>()
-        {
-            const auto prop = static_cast<spark_property_t>(ID);
-            auto propertyNode = spark_create_property_node(prop, Spark::Internal::ThrowOnError());
-
-            const auto dt = static_cast<spark_datatype_t>(TYPE::type);
-            const auto op = static_cast<spark_operator_t>(Spark::Internal::Operator::Property);
-
-            return lvalue<TYPE>(spark_create_operator2_node(dt, op, this->_node, propertyNode));
-        };
-
-        __attribute__((always_inline))
-        lvalue<TYPE> operator()()
-        {
-            const auto prop = static_cast<spark_property_t>(ID);
-            auto propertyNode = spark_create_property_node(prop, Spark::Internal::ThrowOnError());
-
-            const auto dt = static_cast<spark_datatype_t>(TYPE::type);
-            const auto op = static_cast<spark_operator_t>(Spark::Internal::Operator::Property);
-
-            return lvalue<TYPE>(spark_create_operator2_node(dt, op, this->_node, propertyNode));
-        }
-
-        __attribute__((always_inline))
-        property_rw& operator=(const rvalue<TYPE>& right)
-        {
-            Internal::property_assignment_operator(ID, TYPE::type, this->_node, right._node);
-            return *this;
-        }
-    };
 }
