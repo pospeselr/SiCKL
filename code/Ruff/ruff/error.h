5d04f0d0e9a1472bd4f0f99334d56268d95c0788
Richard Pospesel <pospeselr@gmail.com>
2017-04-27 10:34:55 -0700

    made generic version of translate_exception to Ruff for use within Thistle; haven't refactored existing Spark implementation to use Ruff

--- code/Ruff/ruff/error.h
+++ code/Ruff/ruff/error.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdexcept>
+#include <cstdint>
 
 namespace ruff
 {
@@ -60,4 +61,42 @@ namespace ruff
             return get_error_address();
         }
     };
+
+    template<typename error_t>
+    auto translate_exceptions(error_t** error, auto&& lambda) noexcept(true)
+    {
+        try
+        {
+            return lambda();
+        }
+        catch(const std::exception& ex)
+        {
+            if(error)
+            {
+                *error = new error_t(ex.what());
+            }
+        }
+        return decltype(lambda())();
+    }
+
+    inline
+    RUFF_NEVER_INLINE
+    void throw_exception(const char* message, const char* source, uint32_t line)
+    {
+        auto buffer = string_format("%s(%u) : %s", source, line, message);
+        throw std::runtime_error(buffer);
+    }
+
+#define RUFF_THROW_IF_FALSE(X)\
+    do\
+    {\
+        if(!bool(X))\
+        {\
+            ruff::throw_exception(#X, __FILE__, __LINE__);\
+        }\
+    }\
+    while(false)
+
+#define RUFF_THROW_IF_NULL(X) RUFF_THROW_IF_FALSE((X) != nullptr)
+
 }
--- code/Ruff/ruff/utilities.h
+++ code/Ruff/ruff/utilities.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <stdio.h>
+#include <cstdio>
+#include <cstdarg>
+
 #define RUFF_TRACE() printf("%s(%u) : %s\n", __FILE__, (uint32_t)__LINE__, __PRETTY_FUNCTION__)
 
 #define RUFF_UNREFERENCED_PARAMETER(X) (void)(X);
@@ -10,5 +12,23 @@ namespace ruff
     // countof(X)
     template<typename T, size_t N>
     constexpr size_t countof(const T (&)[N]) { return N; }
+
+    inline
+    std::string string_format(const char* format, ...)
+    {
+        va_list args1, args2;
+
+        va_start (args1, format);
+        // vsnprintf destructively modifes the va_list, so make an initial copy
+        va_copy(args2, args1);
+        auto len = vsnprintf(nullptr, 0, format, args1);
+        va_end(args1);
+
+        std::string result(len, 0);
+        vsnprintf(const_cast<char*>(result.data()), len+1, format, args2);
+        va_end(args2);
+
+        return result;
+    }
 }
 
